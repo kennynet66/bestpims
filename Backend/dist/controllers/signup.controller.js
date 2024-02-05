804fa69b53bbf97db70ce23cb855df21af1069e3
Kenedy <kennynet66@gmail.com>
2024-02-05 20:55:43 +0300

    uuid

--- Backend/dist/controllers/signup.controller.js
+++ Backend/dist/controllers/signup.controller.js
@@ -1,10 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.signupController = void 0;
+const uuid_1 = require("uuid");
 let user = [];
 const signupController = (req, res) => {
     const { full_name, email, password } = req.body;
-    let id = "to be filled by uuid";
+    let id = (0, uuid_1.v4)();
     try {
         let newUser = { user_id: id, full_name, email, password };
         user.push(newUser);
--- Backend/src/controllers/signup.controller.ts
+++ Backend/src/controllers/signup.controller.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
 import { signupInterface } from "../interface/signup.interface";
+import { v4 } from 'uuid';
+
 
 let user:signupInterface[] = []
 
 export const signupController = (req:Request, res:Response) => {
     const {full_name, email, password}:signupInterface = req.body;
 
-    let id = "to be filled by uuid";
+    let id = v4();
     try {
         let newUser = {user_id:id, full_name, email, password}
         user.push(newUser);
