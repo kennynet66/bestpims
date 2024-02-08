b9d57c1b459c8a9237be2cad764435097cb8d819
Kenedy <kennynet66@gmail.com>
2024-02-08 11:48:46 +0300

    frontend auth

--- Frontend/dist/admin-login.js
+++ Frontend/dist/admin-login.js
@@ -3,16 +3,86 @@ let userBtn = document.querySelector(".user-btn");
 let loginAdminButton = document.querySelector(".login-btn");
 let forgotPwdRefAdmin = document.querySelector(".forgot-pwd-href");
 let adminForm = document.querySelector("#admin-login-form");
-let admin_email = document.querySelector('#');
+let admin_email = document.querySelector('#email');
+let admin_password = document.querySelector('#password');
+let storedUsers = [];
+window.onload = () => {
+    let data = localStorage.getItem("pimsUser");
+    data = JSON.parse(data);
+    if (!data) {
+        createAdmin();
+        return false;
+    }
+    else {
+        data.forEach((user) => {
+            storedUsers.push(user);
+        });
+        createAdmin();
+    }
+    localStorage.setItem("pimsUser", JSON.stringify(storedUsers));
+};
 userBtn.addEventListener("click", (e) => {
     e.preventDefault();
     window.location.href = "login.html";
 });
-loginAdminButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.location.href = "admin-dashboard.html";
-});
+// loginAdminButton.addEventListener("click",(e)=>{
+//     e.preventDefault();
+//     window.location.href="admin-dashboard.html"
+// });
 forgotPwdRefAdmin.addEventListener("click", (e) => {
     e.preventDefault();
     window.location.href = "forgot-password-admin.html";
 });
+adminForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let isValid = admin_email.value.trim() != "" &&
+        admin_password.value.trim() != "";
+    if (!isValid) {
+        return alert("Please fill in all the fields");
+    }
+    else {
+        let isEmailValid = false;
+        let isPasswordValid = false;
+        let isAdmin = false;
+        console.log("user", storedUsers);
+        storedUsers.forEach((user) => {
+            if (user.status === "admin") {
+                isAdmin = true;
+                if (email.value.trim() === user.email) {
+                    isEmailValid = true;
+                    if (password.value.trim() === user.password) {
+                        isPasswordValid = true;
+                    }
+                }
+            }
+        });
+        if (isAdmin) {
+            if (isEmailValid) {
+                if (isPasswordValid) {
+                    window.location.href = "admin-dashboard.html";
+                    alert("Login success");
+                }
+                else {
+                    alert("Invalid password");
+                }
+            }
+            else {
+                alert("Invalid email");
+            }
+        }
+        else {
+            return alert("not admin email");
+        }
+    }
+});
+function createAdmin() {
+    let newAdmin = {
+        id: 12,
+        fullName: "KenedyMaina",
+        email: "kennynet66@gmail.com",
+        password: "Boomplay@1",
+        status: "admin"
+    };
+    storedUsers.push(newAdmin);
+    localStorage.setItem("pimsUser", JSON.stringify(storedUsers));
+}
--- Frontend/src/admin-login.ts
+++ Frontend/src/admin-login.ts
@@ -3,23 +3,101 @@ let loginAdminButton = document.querySelector(".login-btn") as HTMLButtonElement
 let forgotPwdRefAdmin = document.querySelector(".forgot-pwd-href") as HTMLAnchorElement;
 
 let adminForm = document.querySelector("#admin-login-form") as HTMLFormElement
-let admin_email = document.querySelector('#')
+let admin_email = document.querySelector('#email') as HTMLInputElement;
+let admin_password = document.querySelector('#password') as HTMLInputElement;
+
+let storedUsers: newUser[] = []
+
+window.onload = () => {
+    let data: any = localStorage.getItem("pimsUser")
+    data = JSON.parse(data);
 
 
-userBtn.addEventListener("click",(e)=>{
+    if (!data) {
+        createAdmin();
+        return false
+    } else {
+        data.forEach((user: any) => {
+            storedUsers.push(user)
+        });
+        createAdmin();
+    }
+    localStorage.setItem("pimsUser", JSON.stringify(storedUsers))
+}
+
+userBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
-    window.location.href="login.html"
+    window.location.href = "login.html"
 });
 
-loginAdminButton.addEventListener("click",(e)=>{
+// loginAdminButton.addEventListener("click",(e)=>{
+//     e.preventDefault();
+
+//     window.location.href="admin-dashboard.html"
+// });
+
+forgotPwdRefAdmin.addEventListener("click", (e) => {
     e.preventDefault();
 
-    window.location.href="admin-dashboard.html"
+    window.location.href = "forgot-password-admin.html";
 });
 
-forgotPwdRefAdmin.addEventListener("click",(e)=>{
+adminForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    window.location.href="forgot-password-admin.html";
+
+    let isValid = admin_email.value.trim() != "" &&
+        admin_password.value.trim() != ""
+
+    if (!isValid) {
+        return alert("Please fill in all the fields");
+    } else {
+        let isEmailValid = false;
+        let isPasswordValid = false;
+        let isAdmin = false;
+
+        console.log("user", storedUsers);
+        storedUsers.forEach((user) => {
+
+            if (user.status === "admin") {
+                isAdmin = true;
+                if (email.value.trim() === user.email) {
+                    isEmailValid = true;
+
+                    if (password.value.trim() === user.password) {
+                        isPasswordValid = true;
+                    }
+                }
+            }
+        });
+        if (isAdmin) {
+            if (isEmailValid) {
+                if (isPasswordValid) {
+                    window.location.href = "admin-dashboard.html";
+                    alert("Login success");
+                } else {
+                    alert("Invalid password");
+                }
+            } else {
+                alert("Invalid email");
+            }
+
+        } else {
+            return alert("not admin email")
+        }
+    }
 });
+
+function createAdmin() {
+    let newAdmin: newUser = {
+        id: 12,
+        fullName: "KenedyMaina",
+        email:"kennynet66@gmail.com",
+        password: "Boomplay@1",
+        status: "admin"
+    }
+
+    storedUsers.push(newAdmin);
+    localStorage.setItem("pimsUser", JSON.stringify(storedUsers))
+}
